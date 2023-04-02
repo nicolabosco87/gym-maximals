75fffd68b7f63ecb4ccea0a79db621f99446662a
Nicola Bosco <nicola.bosco87@gmail.com>
2023-04-02 17:41:06 +0200

    Add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,25 @@ export default defineConfig({
             devOptions: {
                 enabled: true,
             },
+            includeAssets: ['favicon.png'],
+            manifest: {
+                name: 'My Awesome App',
+                short_name: 'GymMaximals',
+                description: 'Gym Maximals',
+                theme_color: 'orange',
+                icons: [
+                    {
+                        src: 'icons/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
         }),
     ],
 })
